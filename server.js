2704a9940860bb3a04888f4ca2cd2a76478d47c5
Suraj Singh <76421373+surajaifly@users.noreply.github.com>
2022-01-13 17:08:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ app.post('/favorite', function(req, res) {
 });
 
 app.post('/contact/:id', function(req, res) {
+  console.log('req', req);
   client.query('UPDATE salesforce.contact SET firstname = $2 WHERE sfid = $1', [req.params.id, req.params.lastname], function(error, data) {
     res.json(data.rows[0]);
   });
